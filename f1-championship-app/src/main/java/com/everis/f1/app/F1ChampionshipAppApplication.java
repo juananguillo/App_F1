cfd9d9fafe287e4b209a1d9fc84704ad4290177f
USERSAD\jguillol <jguillol@everis.com>
2021-03-26 14:08:55 +0100

    Creacion del proyecto app F1

---
+++ f1-championship-app/src/main/java/com/everis/f1/app/F1ChampionshipAppApplication.java
@@ -0,0 +1,13 @@
+package com.everis.f1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1ChampionshipAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1ChampionshipAppApplication.class, args);
+	}
+
+}
---
+++ f1-championship-app/src/test/java/com/everis/f1/app/F1ChampionshipAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.f1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1ChampionshipAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
