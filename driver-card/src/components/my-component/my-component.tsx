228ca5847f2ac13aa4a2b32362e89e381ebd049f
USERSAD\jguillol <jguillol@everis.com>
2021-04-23 10:43:36 +0200

    drivercard hecha

---
+++ driver-card/src/components/my-component/my-component.tsx
@@ -0,0 +1,53 @@
+import { Component, Prop, h } from '@stencil/core';
+
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
+})
+export class MyComponent {
+ 
+ 
+  @Prop() picture: string;
+  @Prop() name: string;
+  @Prop() age: string;
+  @Prop() team: string;
+  @Prop() global: number;
+
+ 
+
+  render() {
+  let pos: string| number="";
+  switch (this.global) {
+    case 1:
+      pos="🥇";
+      break;
+   case 2:
+     pos="🥈";
+  
+   case 3:
+     pos="🥉";
+    default:
+      pos=this.global;
+      break;
+  }
+    return<div class="container"><div class="card">
+      <div class="header-driver">
+       <h4>Driver Card</h4>
+       </div>
+      <div class="driver-body">
+      <img class="imgdriver" src={this.picture}></img>
+      <p>Name</p>
+      <p class="content">{this.name}</p>
+      <p>Age</p>
+      <p class="content">{this.age}</p>
+      <p>Team</p>
+      <p class="content">{this.team}</p>
+      <p>Global Ranking</p>
+      <p class="contentpos">{pos}</p>
+      </div>
+    
+  </div></div>;
+  }
+}
